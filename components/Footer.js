a173c333adcc9b181f1c7a3bc491d531fcc4c0ca
mehboss <bairdtim@yahoo.com>
2023-03-05 16:29:19 -0500

    Added contact info

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+        For inquiries please email castrovalvamc@outlook.com
       </footer>
     </>
   )
