e7348b495ebfcd0dbcc2c4db27b338e2bd2422ca
mehboss <bairdtim@yahoo.com>
2023-03-05 16:23:00 -0500

    Edited index

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Website Coming Soon" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Enjin shut down my website
         </p>
       </main>
 
