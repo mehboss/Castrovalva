d107f09d41ea8b552ad1e0347f6e2e9809b80d0b
mehboss <bairdtim@yahoo.com>
2023-03-06 12:22:07 -0500

    fix

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,8 @@
-import styles from './Footer.module.css'
 
 export default function Footer() {
   return (
     <>
-      <footer className={styles.footer}>
+      <footer>
         For inquiries please email castrovalvamc@outlook.com
       </footer>
     </>
