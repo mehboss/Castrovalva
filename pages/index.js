6c8e602a20a61fe261d97625d399ba0cb3843e5c
mehboss <bairdtim@yahoo.com>
2023-03-06 09:53:45 -0500

    Added title

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Castrovalva</title>
+        <link rel="icon" href="/websitelogo.png" />
       </Head>
 
       <main>
         <Header title="Website Coming Soon" />
         <p className="description">
-          Enjin shut down my website <img src="/websitelogo.png" alt="Netlify Logo" className={styles.logo} />
+          My website got shut down my website <img src="/websitelogo.png" alt="Netlify Logo" className={styles.logo} />
         </p>
       </main>
 
